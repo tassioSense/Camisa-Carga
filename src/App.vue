d293d91490cf6f456c20174e7e7d5330c623ce02
TassioSense <tassio.jordao@sensenova.com.br>
2019-05-28 16:32:45 -0300

    Colocando estrutura com vue CLI e primerios componentes

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+   <div id="he"></div>
+   <div id="dados">
+
+      <Dados/>
+
+   </div>
+
+    <div id="results"></div>
+  </div>
+</template>
+
+<script>
+import Dados from './components/Dados'
+
+export default {
+ 
+  components: {
+    Dados
+  }
+}
+</script>
+
+<style>
+body {
+  padding: 0%;
+  margin: 0%;
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(0,255,166,1) 100%);
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 100%;
+
+
+ 
+}
+</style>
